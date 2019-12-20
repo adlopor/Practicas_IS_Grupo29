f0ae8e23082f960bc063bc0997940a8d6845d716
adlopor <lopezortizadrian@gmail.com>
2019-12-20 20:53:03 +0100

    debugging  class Paciente

--- src/paciente.hpp
+++ src/paciente.hpp
@@ -46,17 +46,20 @@ class Paciente:
         std::string 				_fechaNacimiento;                               //!<  \brief Fecha de nacimiento del paciente.
         bool        				_tarjetaSanitaria;                              //!<  \brief Tarjeta sanitaria del paciente.
 
+		int _indiceCitas;
+		int _indiceTratamientos;
+		int _indiceHistorial;
         std::list<is::Cita> 		_citas;				                            //!<  \brief Lista de citas del paciente. 
         std::list<is::Tratamiento> 	_tratamientos;				                    //!<  \brief Lista de tratamientos del paciente.
         std::list<is::Historial> 	_historial;				                        //!<  \brief Lista de los registros de historial del paciente.
 
-	/*!		
+	/*!
 		\name  Métodos públicos de la clase Paciente.
 	*/
 
   	public: 
 
-		/*!	
+		/*!
 			\name Constructor.
 		*/
 
@@ -68,16 +71,14 @@ class Paciente:
             this->setDireccionPostal(direccionPostal);
             this->setFechaNacimiento(fechaNacimiento);
             this->setTarjetaSanitaria(tarjetaSanitaria);
-
-			this->		
-  		}
+		}
 
 
-		/*!	
+		/*!
 			\name Observadores
 		*/
 
-		/*!		
+		/*!
 			\brief  Devuelve el nombre del paciente
 			\note   Función de tipo inline
 			\return cadena que representa el nombre del paciente  
@@ -85,7 +86,7 @@ class Paciente:
 		*/
 		inline int getNombre() const{return _nombre;}
 
-		/*!		
+		/*!
 			\brief  Devuelve los apellidos del paciente
 			\note   Función de tipo inline
 			\return cadena que representa los apellidos del paciente 
@@ -93,7 +94,7 @@ class Paciente:
 		*/
 		inline std::string getApellidos() const{return _apellidos;}
 
-        /*!		
+        /*!
 			\brief  Devuelve el número de teléfono del paciente
 			\note   Función de tipo inline
 			\return cadena que representa el número de teléfono del paciente 
@@ -101,7 +102,7 @@ class Paciente:
 		*/
 		inline std::string getTelefono() const{return _telefono;}
 
-        /*!		
+        /*!
 			\brief  Devuelve la dirección postal del paciente
 			\note   Función de tipo inline
 			\return cadena que representa la dirección Postal del paciente 
@@ -109,7 +110,7 @@ class Paciente:
 		*/
 		inline std::string getDireccionPostal() const{return _direccionPostal;}
 
-        /*!		
+        /*!
 			\brief  Devuelve la fecha de nacimiento del paciente
 			\note   Función de tipo inline
 			\return cadena que representa la fecha de nacimiento del paciente 
@@ -117,7 +118,7 @@ class Paciente:
 		*/
 		inline std::string getFechaNacimiento() const{return _fechaNacimiento;}
 
-        /*!		
+        /*!
 			\brief  Devuelve si el paciente tiene tarjeta sanitaria
 			\note   Función de tipo inline
 			\return Booleano que representa con "True" si el paciente paciente tiene tarjeta sanitaria. En caso de no tener devuelve "False". 
@@ -125,13 +126,13 @@ class Paciente:
 		*/
 		inline bool getTarjetaSanitaria() const
         {
-            if (_tarjetaSanitaria)
+        	if (_tarjetaSanitaria)
                 return true;
             else
                 return false;
         }
 
-		/*!		
+		/*!
 			\brief  Devuelve la lista de citas del paciente
 			\note   Función de tipo inline
 			\return Listado de citas del paciente 
@@ -139,7 +140,7 @@ class Paciente:
 		*/
 		inline std::list<is::Cita> getCitas() const{this->_citas;}
 
-		/*!		
+		/*!
 			\brief  Devuelve la lista de tratamientos del paciente
 			\note   Función de tipo inline
 			\return Listado de tratamientos del paciente 
@@ -147,7 +148,7 @@ class Paciente:
 		*/
 		inline std::list<is::Tratamiento> getTratamientos() const{this->_tratamientos;}
 
-		/*!		
+		/*!
 			\brief  Devuelve la lista de historial del paciente
 			\note   Función de tipo inline
 			\return Listado de historial del paciente 
@@ -159,7 +160,7 @@ class Paciente:
 			\name Modificadores
 		*/
 
-		/*!		
+		/*!
 			\brief  Modifica el nombre del paciente
   			\note   Función inline
 			\param  nombre: cadena que será el nuevo nombre del paciente
@@ -317,67 +318,73 @@ class Paciente:
 			#endif
 		}
 
-
-    /*!	
-	        \name Operadores
-    */
-
-    /*!		
-	        \brief  Operador de asignacion entre objetos de tipo Paciente
-	        \param  p: objeto de tipo Paciente
-	        \post   El objeto actual es igual al objeto parámetro
-	        \return Devuelve una referencia al objeto actual
-    */
-	Tratamiento &operator=(const Paciente &p);
-
-    /*!		
-	        \brief  Operador que compara la igualdad lexicográfica por el nombre, los apellidos, teléfono, dirección postal, fecha de nacimiento y tarjeta sanitaria
-	        \param  p: objeto de tipo Paciente
-	        \pre    Ambas citas deben tener valores en los parámetros; el nombre, los apellidos, teléfono, dirección postal, fecha de nacimiento y tarjeta sanitaria
-			\post   Ninguna
-    	    \return Devuelve true si el nombre, los apellidos, teléfono, dirección postal , fecha de nacimiento y tarjeta sanitaria de los pacientes son iguales. False, en caso contrario
-    */
-	bool operator==(const Paciente &p) const;
-
-    /*!
-	        \brief  Operador que compara la desigualdad lexicográfica por nombre y apellidos de dos pacientes.
-	        \param  p: objeto de tipo Paciente
-	        \pre    Ambos pacientes deben tener valores en los parámetros nombre y apellido.
-	        \post   Ninguna
-	        \return Devuelve true si el paciente actual precede cronológicamente al paciente pasado como argumento. False, en caso contrario
-    */
-	bool operator<(const Paciente &p) const;
-
-
-    /*!
-            \name Funciones amigas para poder acceder a la parte privada de la clase Paciente.
+	/*!	
+	        \name Métodos del Listado de Citas
     */
 
     /*!
-	        \brief  Operador de inserción
-	        \param  i: flujo de entrada
-	        \param  p: objeto de tipo Paciente
-	        \pre    El paciente del parámetro debe existir
-	        \post   Se modifican los atributos del paciente usando los valores insertados por el flujo de entrada
-	        \return Devuelve el stream de entrada
+	    \brief  Método que devuelve el tamaño del listado de citas
+		\note	Función inline
+	    \return Devuelve un valor entero con el tamaño de la lista de citas
     */
-	friend std::istream &operator>>(std::istream &i, Paciente &p);
+	inline int getTamanoListaCitas(){return this->_citas.size();}
 
-    /*!		
-	    	\brief  Operador de extracción
-	        \param  o: flujo de salida
-	        \param  p: objeto de tipo Paciente
-    	    \pre    El paciente del parámetro debe existir
-	        \post   Se escriben los valores de los atributos del paciente en flujo de salida
-	        \return Devuelve el stream de salida
-	*/
-	friend std::ostream &operator<<(std::ostream &o, Paciente const &p);
+	/*!
+	    \brief  Método que indica si la lista de citas está vacía
+		\note	Función inline
+	    \return Devuelve un booleano de valor True en caso de que la lista esta vacía. False, en caso contrario.
+		\sa		getTamanoListaCitas
+    */
+	inline bool estaVaciaListaCitas()
+	{
+		if(this->_citas.empty()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/*!
+	    \brief  Método que devuelve la posición en la que se encuentra el índice de la lista de citas
+	    \note	Función Inline
+		\return Devuelve un entero que indica la posición del índice de la lista de citas
+    */
+	inline int getIndiceListaCitas (){return this->_indiceCitas;}
 
- 		/*!	
-			\name Otros métodos de la clase Cita
-		*/
+	/*!
+	    \brief  Método que ajusta la posición en la que se encuentra el índice de la lista de citas al inicio
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaCitas
+    */
+	inline void setIndiceListaCitasBegin (){this->_indiceCitas = 0;}
 
-	/*!		
+	/*!
+	    \brief  Método que ajusta la posición en la que se encuentra el índice de la lista de citas al final
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaCitas
+    */
+	inline void setIndiceListaCitasEnd (){this->_indiceCitas = getTamanoListaCitas();}
+	
+	/*!
+	    \brief  Método que avanza en una posición el índice de la lista de citas
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaCitas
+    */
+	inline void avanzarCitaAdelante (){this->_indiceCitas = (this->_indiceCitas + 1) % getTamanoListaCitas();}
+	
+	/*!
+	    \brief  Método que devuelve la cita señalada por el índice del listado de citas
+		\pre	La lista no puede estar vacía
+	    \return Devuelve la cita del listado de citas a la que esté señalando el índice del listado de citas
+		\sa		estaVaciaListaCitas
+    */
+	is::Cita getCurrentCita();
+
+	/*!
 		\brief  Buscar cita en la lista de citas del paciente
   		\param  c: variable de la clase Cita
 		\return Si encuentra la cita devuelve true. En caso contrario, devuelve false.
@@ -385,7 +392,7 @@ class Paciente:
 	*/
 	bool buscarCita (is::Cita &c);
 
-	/*!		
+	/*!
 		\brief  Carga la cita de la lista de citas en una clase de tipo Cita
 		\param  c: variable de la clase Cita
 		\return Devuelve la cita que se quiere cargar. Si no existe, la última cita de la lista, junto con un mensaje de error.
@@ -393,7 +400,7 @@ class Paciente:
 	*/
 	is::Cita cargarCita (is::Cita &c);
 
-	/*!		
+	/*!
 		\brief  Guarda la cita en la lista de citas del paciente y la reordena.
   		\param  c: variable de la clase Cita
 		\return No se devuelve ningún resultado
@@ -401,7 +408,7 @@ class Paciente:
 	*/
 	void guardarCita (is::Cita &c);
 
-	/*!		
+	/*!
 		\brief  Borra una cita de la lista de citas del paciente
   		\param  c: variable de la clase Cita
 		\return No se devuelve ningún resultado
@@ -410,46 +417,173 @@ class Paciente:
 	void borrarCita (is::Cita &c);
 
 
- 		/*!	
-			\name Otros métodos de la clase Tratamiento
-		*/
+	/*!
+	    \name Métodos del Listado de Tratamientos
+    */
 
-	/*!		
+    /*!
+	    \brief  Método que devuelve el tamaño del listado de tratamientos
+		\note	Función inline
+	    \return Devuelve un valor entero con el tamaño de la lista de tratamientos
+    */
+	inline int getTamanoListaTratamientos(){return this->_tratamientos.size();}
+
+	/*!
+	    \brief  Método que indica si la lista de tratamientos está vacía
+		\note	Función inline
+	    \return Devuelve un booleano de valor True en caso de que la lista esta vacía. False, en caso contrario.
+		\sa		getTamanoListaTratamientos
+    */
+	inline bool estaVaciaListaTratamientos()
+	{
+		if(this->_tratamientos.empty())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	/*!
+	    \brief  Método que devuelve la posición en la que se encuentra el índice de la lista de tratamientos
+	    \note	Función Inline
+		\return Devuelve un entero que indica la posición del índice de la lista de tratamientos
+    */
+	inline int getIndiceListaTratamientos (){return this->_indiceTratamientos;}
+
+	/*!
+	    \brief  Método que ajusta la posición en la que se encuentra el índice de la lista de tratamientos al inicio
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaTratamientos
+    */
+	inline int setIndiceListaTratamientosBegin (){this->_indiceTratamientos = 0;}
+
+	/*!
+	    \brief  Método que ajusta la posición en la que se encuentra el índice de la lista de tratamientos al final
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaTratamientos
+    */
+	inline int setIndiceListaTratamientosEnd (){this->_indiceTratamientos = getTamanoListaTratamientos();}
+	
+	/*!
+	    \brief  Método que avanza en una posición el índice de la lista de tratamientos
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaTratamientos
+    */
+	inline void avanzarTratamientoAdelante (){this->_indiceTratamiento = (this->_indiceTratamiento + 1) % getTamanoListaTratamientos();}
+	
+	/*!
+	    \brief  Método que devuelve el tratamiento señalado por el índice del listado de tratamientos
+		\pre	La lista no puede estar vacía
+	    \return Devuelve el tratamiento del listado de tratamientos al que esté señalando el índice
+		\sa		estaVaciaListaTratamientos
+    */
+	is::Tratamiento getCurrentTratamiento ();
+
+	/*!	
 		\brief  Buscar tratamiento en la lista de tratamientos del paciente
   		\param  t: variable de la clase Tratamiento.
 		\return Si encuentra el tratamiento devuelve true. En caso contrario, devuelve false.
-  		
 	*/
 	bool buscarTratamiento (is::Tratamiento &t);
 
-	/*!		
+	/*!
 		\brief  Carga el tratamiento de la lista de tratamientos en una clase de tipo Tratamiento
 		\param  t: variable de la clase Tratamiento
 		\return Devuelve el tratamiento que se quiere cargar. Si no existe, devuelve el último tratamiento de la lista, junto con un mensaje de error.
-  		
+  		\sa 	buscarTratamiento
 	*/
-	is::Cita cargarTratamiento (is::Tratamiento &t);
+	is::Tratamiento cargarTratamiento (is::Tratamiento &t);
 
-	/*!		
+	/*!
 		\brief  Guarda el tratamiento en la lista de tratamientos del paciente y la reordena.
   		\param  t: variable de la clase Tratamiento
 		\return No se devuelve ningún resultado
-  		
+  		\sa 	buscarTratamiento
 	*/
 	void guardarTratamiento (is::Tratamiento &t);
 
-	/*!		
+	/*!
 		\brief  Borra un tratamiento de la lista de tratamientos del paciente
   		\param  t: variable de la clase Tratamiento
 		\return No se devuelve ningún resultado
-  		
+		\sa		buscarTratamiento
 	*/
 	void borrarTratamiento (is::Tratamiento &t);
 
 
- 		/*!	
-			\name Otros métodos de la clase Historial
-		*/
+	/*!
+	    \name Métodos del Listado de Historial
+    */
+
+    /*!
+	    \brief  Método que devuelve el tamaño del listado de historial
+		\note	Función inline
+	    \return Devuelve un valor entero con el tamaño de la lista de historial
+    */
+	inline int getTamanoListaHistorial(){return this->_historial.size();}
+
+	/*!
+	    \brief  Método que indica si la lista de historial está vacía
+		\note	Función inline
+	    \return Devuelve un booleano de valor True en caso de que la lista esta vacía. False, en caso contrario.
+		\sa		getTamanoListaHistorial
+    */
+	inline bool estaVaciaListaHistorial()
+	{
+		if(this->_historial.empty())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	/*!
+	    \brief  Método que devuelve la posición en la que se encuentra el índice de la lista de historial
+	    \note	Función Inline
+		\return Devuelve un entero que indica la posición del índice de la lista de historial
+    */
+	inline int getIndiceListaHistorial (){return this->_indiceHistorial;}
+
+	/*!
+	    \brief  Método que ajusta la posición en la que se encuentra el índice de la lista de historial al inicio
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaHistorial
+    */
+	inline int setIndiceListaHistorialBegin (){this->_indiceHistorial = 0;}
+
+	/*!
+	    \brief  Método que ajusta la posición en la que se encuentra el índice de la lista de historial al final
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaHistorial
+    */
+	inline int setIndiceListaHistorialEnd (){this->_indiceHistorial = getTamanoListaHistorial());}
+	
+	/*!
+	    \brief  Método que avanza en una posición el índice de la lista de historial
+	    \note	Función Inline
+		\return void
+		\sa		getIndiceListaHistorial
+    */
+	inline void avanzarHistorialAdelante (){this->_indiceHistorial = (this->_indiceHistorial + 1) % getTamanoListaHistorial();}
+	
+	/*!
+	    \brief  Método que devuelve el historial señalado por el índice del listado de historiales
+		\pre	La lista no puede estar vacía
+	    \return Devuelve el historial del listado de historiales al que esté señalando el índice
+		\sa		estaVaciaListaHistoriales
+    */
+	is::Historial getCurrentHistorial();
 
 	/*!		
 		\brief  Buscar historial en la lista de historiales del paciente
@@ -465,7 +599,7 @@ class Paciente:
 		\return Devuelve el historial que se quiere cargar. Si no existe, devuelve el último historial de la lista, junto con un mensaje de error.
   		
 	*/
-	is::Cita cargarHistorial (is::Historial &h);
+	is::Historial cargarHistorial (is::Historial &h);
 
 	/*!		
 		\brief  Guarda el historial en la lista de historiales del paciente y la reordena.
@@ -476,13 +610,68 @@ class Paciente:
 	void guardarHistorial (is::Historial &h);
 
 	/*!		
-		\brief  Borra un historial de la lista de hsitoriales del paciente
+		\brief  Borra un historial de la lista de historiales del paciente
   		\param  h: variable de la clase Historial
 		\return No se devuelve ningún resultado
   		
 	*/
 	void borrarHistorial (is::Historial &h);
 
+
+    /*!	
+        \name Operadores
+    */
+
+    /*!		
+        \brief  Operador de asignacion entre objetos de tipo Paciente
+        \param  p: objeto de tipo Paciente
+        \post   El objeto actual es igual al objeto parámetro
+        \return Devuelve una referencia al objeto actual
+    */
+	Tratamiento &operator=(const Paciente &p);
+
+    /*!		
+        \brief  Operador que compara la igualdad lexicográfica por el nombre, los apellidos, teléfono, dirección postal, fecha de nacimiento y tarjeta sanitaria
+        \param  p: objeto de tipo Paciente
+        \pre    Ambas citas deben tener valores en los parámetros; el nombre, los apellidos, teléfono, dirección postal, fecha de nacimiento y tarjeta sanitaria
+		\post   Ninguna
+   	    \return Devuelve true si el nombre, los apellidos, teléfono, dirección postal , fecha de nacimiento y tarjeta sanitaria de los pacientes son iguales. False, en caso contrario
+    */
+	bool operator==(const Paciente &p) const;
+
+    /*!
+        \brief  Operador que compara la desigualdad lexicográfica por nombre y apellidos de dos pacientes.
+        \param  p: objeto de tipo Paciente
+        \pre    Ambos pacientes deben tener valores en los parámetros nombre y apellido.
+        \post   Ninguna
+        \return Devuelve true si el paciente actual precede cronológicamente al paciente pasado como argumento. False, en caso contrario
+	*/
+	bool operator<(const Paciente &p) const;
+
+
+    /*!
+        \name Funciones amigas para poder acceder a la parte privada de la clase Paciente.
+    */
+
+    /*!
+        \brief  Operador de inserción
+        \param  i: flujo de entrada
+        \param  p: objeto de tipo Paciente
+        \pre    El paciente del parámetro debe existir
+        \post   Se modifican los atributos del paciente usando los valores insertados por el flujo de entrada
+        \return Devuelve el stream de entrada
+    */
+	friend std::istream &operator>>(std::istream &i, Paciente &p);
+
+    /*!		
+    	\brief  Operador de extracción
+        \param  o: flujo de salida
+        \param  p: objeto de tipo Paciente
+   	    \pre    El paciente del parámetro debe existir
+        \post   Se escriben los valores de los atributos del paciente en flujo de salida
+        \return Devuelve el stream de salida
+	*/
+	friend std::ostream &operator<<(std::ostream &o, Paciente const &p);
 
 		/*!	
 			\name Otros métodos de la clase Paciente
